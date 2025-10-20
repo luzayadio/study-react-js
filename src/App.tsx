a0cc2b19b3c1ed022a69b18ae5e2f18e141981f6
Luzayadio Vila <71708339+luzayadio@users.noreply.github.com>
2025-10-20 15:06:51 +0100

    refresh mind about funcions & hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState } from "react"
 
 
 export function App() {
   return (
-    <>
+    <div>
       <h2>Hello world</h2>
-    </>
+    </div>
+  )
+}
+
+// Função JS
+const teste = () => {
+  return 1 + 1
+}
+
+//Função JS
+const useTest = () => {
+  return 1 + 1
+}
+
+// React Hook
+const useTestHook = () => {
+  const [value] = useState(1 + 1)
+  return value
+}
+
+// Component
+/* As componentes devem começar sempre com letra maiuscula 
+para diferenciar dos componente do React JSX */
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>This is my Component</h1>
+    </div>
   )
 }
