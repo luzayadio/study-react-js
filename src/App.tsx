afe7e46fd41ae19797b1a39867fd69282f191ee1
Luzayadio Vila <71708339+luzayadio@users.noreply.github.com>
2025-10-20 21:02:32 +0100

    Component, props Interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
-import { useState } from "react"
+interface ICardProps {
+  title: string;
+  children: React.ReactNode;
+}
+const Card = (props: ICardProps) => {
+  return (
+    <div style={{ border: "1px solid black", margin: "15px 10px 15px 10px" }}>
+      <span>Title: {props.title}</span>
+
+      <div>{props.children}</div>
+
+      <div>Footer</div>
+    </div>
+  )
+}
+
 
 
 export function App() {
@@ -6,59 +21,13 @@ export function App() {
     <div>
       <h2>Hello world</h2>
 
-      {teste()}
-      <br />
-      {useTest()}
-      <br />
-      {useTestHook()}
-      <br />
-
-      {MyComponent()}
-      <MyComponent></MyComponent>
-      <MyName></MyName>
-    </div>
-  )
-}
-
-// Função JS
-const teste = () => {
-  return 1 + 1
-}
-
-//Função JS
-const useTest = () => {
-  return 1 + 1
-}
-
-// React Hook
-const useTestHook = () => {
-  const [value] = useState(1 + 1)
-  return value
-}
-
-// Component
-/* As componentes devem começar sempre com letra maiuscula 
-para diferenciar dos componente do React JSX */
-const MyComponent = () => {
-  return (
-    <div>
-      <h1>This is my Component</h1>
-    </div>
-  )
-}
-
-function MyName() {
-  return (
-    <div>
-      <h2>My name is Vila</h2>
-    </div>
-  )
-}
-
-const MyAge = (age: number) => {
-  return (
-    <div>
-      <h2>My name is {age}</h2>
+      <p>Card:</p>
+      <Card title="Surrender">
+        Testando apenas
+      </Card>
+      <Card title="Surrender">
+        Testando apenas
+      </Card>
     </div>
   )
 }
