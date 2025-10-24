2222c7ec7a5fa55044c73183fa52746438376552
Luzayadio Vila <71708339+luzayadio@users.noreply.github.com>
2025-10-24 23:01:45 +0100

    Renderização condicional no REACT

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-interface ICardProps {
-  title: string;
-  children: React.ReactNode;
-}
-const Card = (props: ICardProps) => {
-  return (
-    <div style={{ border: "1px solid black", margin: "15px 10px 15px 10px" }}>
-      <span>Title: {props.title}</span>
-
-      <div>{props.children}</div>
-
-      <div>Footer</div>
-    </div>
-  )
-}
-
-
+import { useState } from "react";
 
 export function App() {
+  const [hide, setHide] = useState(false)
+  
+  if(hide) return (<h1>null</h1>)
+  // Nunca colocar um hook(useState, etc) depois de um if com return, vai causar erro!!
+
   return (
     <div>
-      <h2>Hello world</h2>
+      {hide && <h2>Hello world 1</h2>}
+      {!hide && <h2>Hello world 2</h2>}
 
-      <p>Card:</p>
-      <Card title="Surrender">
-        Testando apenas
-      </Card>
-      <Card title="Surrender">
-        Testando apenas
-      </Card>
+      {hide ? <h2>Hello world 3</h2> : <h2>Hello world 4</h2>}
+
+
+      <button onClick={() => setHide(!hide)}>
+        {hide ? "Impar" : "Par"}
+      </button>
     </div>
   )
 }
